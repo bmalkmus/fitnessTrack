9c6b13ee79349c93b83dfa331f371d6681e2f7bd
bmalkmus <bmalkmus23@gmail.com>
2020-04-08 12:43:38 -0700

    schema added

---
+++ models/tracker.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema ({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: {
+        type:{
+            type: String,
+            trim: true,
+            required: true
+        },
+        name:{
+            type: String,
+            trim: true,
+            required: "Enter a name of the exercise"
+        },
+        duration:{
+            type: Number,
+            required: "Enter number of minutes it took to complete the exercise"
+        },
+        weight: {
+            type: Number
+        },
+        reps:{
+            type: Number
+        },
+        sets:{
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }
+})
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
